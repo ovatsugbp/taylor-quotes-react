9a54b92632c28061c24e0810e549332673eecfdc
Gustavo G <gustavogoullart@hotmail.com>
2020-12-15 16:34:33 -0300

    responsive adjustments

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,43 +1,46 @@
-import React, { Component } from 'react';
-import Typography from '../../Components/Typography';
-import Button from '../../Components/Button';
+import React, { Component } from "react";
+import Typography from "../../Components/Typography";
+import Button from "../../Components/Button";
 import api from "../../services/api";
-import './style.css';
+import "./style.css";
 
 export default class Main extends Component {
-    state = {
-        quotes: [],
-        image: []
-    }
+  state = {
+    quotes: [],
+    image: [],
+  };
 
-    componentDidMount(){
-        this.loadSwift();
-    }
+  componentDidMount() {
+    this.loadSwift();
+  }
 
-    loadSwift = async () => {
-        const responseQuote = await api.get("/");
-        const responseImage = await api.get("/image");
-        
-        this.setState(
-            {
-                quotes: responseQuote.data.quote,
-                image: responseImage.data.url
-        });
-        
-    };
+  loadSwift = async () => {
+    const responseQuote = await api.get("/");
+    const responseImage = await api.get("/image");
 
-    render() {
-        const {quotes, image} = this.state;
-        return (
-            <div className="taylor-data">                
-            <Typography variant="subheading" color="primary">{quotes}</Typography>
-            <img src={image} alt="Taylor Swift"></img>
-            <Button onClick={this.loadSwift}
-                type="button"
-                buttonStyle="btn--solid"
-                buttonSize="btn--campaigns--lg"
-            >give me another round</Button>
-            </div>
-        )
-    }
+    this.setState({
+      quotes: responseQuote.data.quote,
+      image: responseImage.data.url,
+    });
+  };
+
+  render() {
+    const { quotes, image } = this.state;
+    return (
+      <div className="taylor-data">
+        <Typography variant="subheading" color="primary">
+          {quotes}
+        </Typography>
+        <img src={image} alt="Taylor Swift"></img>
+        <Button
+          onClick={this.loadSwift}
+          type="button"
+          buttonStyle="btn--solid"
+          buttonSize="btn--campaigns--lg"
+        >
+          give me another round
+        </Button>
+      </div>
+    );
+  }
 }
